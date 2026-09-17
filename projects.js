d256dc8767fdb5e79790753ffbdfe90e10b626a7
Minacce23 <kenkenrose23@gmail.com>
2026-09-16 21:18:38 -0700

    Update projects from admin page

--- projects.js
+++ projects.js
@@ -5,6 +5,7 @@
 // date: "YYYY-MM", or "" to hide it. hidden: true keeps a project off the site.
 // Projects appear on the site in this order.
 const PROJECTS = [
+  {"title":"Powerpuff Sweater","categories":["crochet"],"date":"2022-01","photos":["crochet-mu50rwfrmr06.jpg"]},
   {"title":"Froggy Hat","categories":["sewing"],"date":"2021-12","photos":["sewing-mu50gaexgdba.jpg","sewing-mu50gagdm2xs.jpg"]},
   {"title":"Euphoria Sweater Vest + Collared Shirt","categories":["sewing","crochet"],"date":"2020-10","photos":["sewing-mu50e7ml99q0.jpg","sewing-mu50e7kybt72-emu50eez5.jpg"],"photoTags":{"sewing-mu50e7kybt72-emu50eez5.jpg":["crochet"]}},
   {"title":"Leaf Cardigan","categories":["knitting"],"date":"2023-09","photos":["knitting-mu50d0a476zh.jpg"]},
